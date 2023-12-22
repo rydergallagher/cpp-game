e62b712cf92d0e40816b78109e5f9f6bbd7c0589
rydergallagher <rgallagher03@yahoo.com>
2023-12-22 14:10:32 -0800

    completed calculateDamage function

--- calculateDamage.cpp
+++ calculateDamage.cpp
@@ -2,6 +2,11 @@
 
 int calculateDamage(Character attacker, Character target){
 
+    int attackerStrength = attacker.getStrength();
+    int targetDefense = target.getDefense();
 
+    int result = attackerStrength - targetDefense;
+
+    return result;
 
 }
