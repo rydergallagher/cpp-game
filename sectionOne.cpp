c72f676b71f518daa8747787b444cdc26472eb97
rydergallagher <rgallagher03@yahoo.com>
2023-12-26 20:48:02 -0800

    Issue with mainCharacter being passed into the function

--- sectionOne.cpp
+++ sectionOne.cpp
@@ -2,4 +2,9 @@
 
 void sectionOne(Character mainCharacter()){
 
+    std::string tempName;
+    std::cout << "Enter your name: ";
+    std::cin >> tempName;
+    mainCharacter.setName(tempName);
+
 }
